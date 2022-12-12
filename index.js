82c0e5212387e491e33c746bcd264f814381a1c1
Thomas <tfnielson.se@gmail.com>
2022-12-12 11:00:43 -0700

    commit tree 5aa4f782f63302e1f650b7c09de8c1820c21c028

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = (`thomas`);
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase ();
+
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
